57f43f6e5d0041adc00dbbb1b39e1ff98e0ca57f
David Kleinberg <davidkleinberg@jhu.edu>
2017-11-12 15:23:46 -0500

    Replace video with message frames and fix embedded link

--- main.js
+++ main.js
@@ -60,6 +60,79 @@ function shootBall() {
     }
 }
 
+function displayMessage() {
+    for (var i = 1; i < 52; i++) {
+        var screen = document.createElement("img");
+        screen.setAttribute("src", "./Message/Message"+i+".png");
+        screen.setAttribute("height", "185.75px");
+        screen.setAttribute("id", "message"+i);
+        screen.setAttribute("alt", "Message");
+
+        // screen.style.position = "absolute";
+        // screen.style.left = "0";
+        // screen.style.right = "0";
+        // screen.style.margin = "auto";
+
+        screen.style.position = "absolute";
+        screen.style.left = "50%";
+        screen.style.marginLeft = "-151px";
+        screen.style.top = "50%";
+        screen.style.marginTop = "16px";
+        screen.style.visibility = "hidden";
+        screen.style.zIndex = "-1";
+
+        document.getElementById("moving_content").appendChild(screen);
+
+        playMessage();
+    }
+}
+
+function playMessage() {
+    console.log("message"); 
+    var elem = null;
+    var inc = 11;
+    var index = 1;
+    var id = setInterval(frame, .5);
+    function frame() {
+        if (index != 52) {
+            if (inc == 11) {
+                if (index == 51) {
+                    index = 1;
+                } else {
+                    index++;
+                }
+                elem = document.getElementById("message"+index);
+                elem.style.visibility = 'visible'; //visible
+                inc = 0;
+                //clearInterval(id);
+            } else if (inc == 10) {
+                elem.style.visibility = 'hidden'; //hidden
+                inc++;
+            } else {
+                inc++; 
+            }
+        }
+    }
+}
+
+
+function bringForward() {
+    var button = document.getElementById("computer_backdrop");
+    var video = document.getElementById("computer_video");
+    button.style.zIndex = '2';
+    video.style.zIndex = '3'
+
+}
+function bringBackward() {
+    var button = document.getElementById("computer_backdrop");
+    var video = document.getElementById("computer_video");
+    button.style.zIndex = '-1';
+    video.style.zIndex = '-1'
+}
+function github() {
+     window.location.href = "https://github.com/DavidKleinberg/";
+}
+
 var figure = $(".computer_video").hover( hoverVideo, hideVideo );
 
 function hoverVideo(e) {  
